82bceb4da21884a041cf82d23b3d01521d6c2299
Chase Wilcoxen <chasewilcoxen@chases-mbp.myfiosgateway.com>
2023-04-17 11:36:51 -0400

    migrating to newer version of angular

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,45 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
+import { MatDividerModule} from "@angular/material/divider"
+import { MatIconModule } from "@angular/material/icon"
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule} from '@angular/material/list';
+import { AreaComponent } from './widget/area/area.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    AreaComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatListModule,
+    RouterModule,
+    HighchartsChartModule
+
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    AreaComponent,
+  ]
+})
+export class SharedModule { }
---
+++ src/app/shared/widget/area/area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import * as HighCharts from 'highcharts'
+@Component({
+  selector: 'app-widget-area',
+  templateUrl: './area.component.html',
+  styleUrls: ['./area.component.scss']
+})
+export class AreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
